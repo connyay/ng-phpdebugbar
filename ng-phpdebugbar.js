bc2157b1cc14645a927f637e90620f1db8de23e0
Connor Hindley <conn.hindley@gmail.com>
2014-07-06 22:16:03 -0400

    initial commit

---
+++ ng-phpdebugbar.js
@@ -0,0 +1,33 @@
+(function() {
+    'use strict';
+    var getDebugBarID = function(response) {
+        var headers = response && response.headers && response.headers();
+        var headerName = phpdebugbar.ajaxHandler.headerName + '-id';
+        return headers[headerName] || false;
+    };
+    angular.module('ng-phpdebugbar', [])
+        .factory('phpDebugBarInterceptor', function() {
+            return {
+                'request': function(config) {
+                    // Debugbar looks for this header to send back the debugbar id
+                    config.headers['X-Requested-With'] = 'XMLHttpRequest';
+                    return config;
+                },
+                'response': function(response) {
+                    if (phpdebugbar && phpdebugbar.ajaxHandler) {
+                        var debugBarID = getDebugBarID(response);
+                        if (debugBarID) {
+                            phpdebugbar.loadDataSet(debugBarID, ('ajax'));
+                        }
+                    }
+                    return response;
+                }
+            };
+        })
+        .config(['$httpProvider',
+            function($httpProvider) {
+                $httpProvider.interceptors.push('phpDebugBarInterceptor');
+            }
+        ]);
+
+})();
