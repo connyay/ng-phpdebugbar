baa368531a619399f7bffd1edd970f58108229ce
Connor Hindley <conn.hindley@gmail.com>
2014-07-07 10:30:48 -0400

    add comments

--- ng-phpdebugbar.js
+++ ng-phpdebugbar.js
@@ -3,23 +3,31 @@
     var getDebugBarID = function(response) {
         var headers = response && response.headers && response.headers();
         if (!headers) {
-            return false;
+            // Something terrible happened. Bail.
+            return;
         }
+        // Not very elegant, but this is how the debugbar.js defines the header.
         var headerName = phpdebugbar.ajaxHandler.headerName + '-id';
-        return headers[headerName] || false;
+        return headers[headerName];
     };
     angular.module('ng-phpdebugbar', [])
         .factory('phpDebugBarInterceptor', function() {
             return {
                 'request': function(config) {
-                    // Debugbar looks for this header to send back the debugbar id
+                    // This is the header that debugbar looks for that triggers
+                    // the debugbarid to be returned in the response headers.
                     config.headers['X-Requested-With'] = 'XMLHttpRequest';
                     return config;
                 },
                 'response': function(response) {
                     if (phpdebugbar && phpdebugbar.ajaxHandler) {
+                        // We have a debugbar and an ajaxHandler
+                        // Dig through response to look for the 
+                        // debugbar id.
                         var debugBarID = getDebugBarID(response);
                         if (debugBarID) {
+                            // A debugBarID was found! Now we just pass the
+                            // id to the debugbar to load the data
                             phpdebugbar.loadDataSet(debugBarID, ('ajax'));
                         }
                     }
@@ -29,6 +37,7 @@
         })
         .config(['$httpProvider',
             function($httpProvider) {
+                // Adds our debug interceptor to all $http requests
                 $httpProvider.interceptors.push('phpDebugBarInterceptor');
             }
         ]);
